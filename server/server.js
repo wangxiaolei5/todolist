52469b52f05157905677c3da07f97ac933321d82
王晓磊 <wangxiaolei@happyplaygame.net>
2019-06-20 17:25:55 +0800

    api

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const Koa = require('koa')
 const send = require('koa-send')
 const path = require('path')
+const koaBody = require('koa-body')
 // const pageRouter = require('./routers/dev-ssr')
 const staticRouter = require('./routers/static')
 const apiRouter = require('./routers/api')
@@ -36,6 +37,7 @@ app.use(async (ctx, next) => {
   ctx.db = db
   await next()
 })
+app.use(koaBody())
 app.use(staticRouter.routes()).use(staticRouter.allowedMethods())
 app.use(apiRouter.routes()).use(apiRouter.allowedMethods())
 let pageRouter
